e9190a8e9373371578689582e82f31086931f278
Minos <mindaugas.andrulevicius@gmail.com>
2025-01-29 21:24:18 +0200

    refactor and fix spider

--- Assets/_MA/ProceduralAnimation.cs
+++ Assets/_MA/ProceduralAnimation.cs
@@ -1,36 +1,60 @@
 using System.Collections.Generic;
+using Unity.IntegerTime;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ProceduralAnimation : MonoBehaviour
 {
     // maybe make a struct for each leg
+    [SerializeField] GameObject player;
+    [SerializeField] float speed = 5f;
 
-    [SerializeField] private List<GameObject> thighHips;
-    [SerializeField] private List<GameObject> legTargets;
-    
-    private List<Vector3> _localRestPositions = new ();
-    private List<Vector3> _snapPositions = new ();
-    private List<Vector3> _rayDirections = new ();
-    private List<Ray> _rays = new ();
-    private Ray _tempRay;
-    private RaycastHit _hit;
-    
-    [SerializeField] private float maxDistance = 2;
-    [SerializeField] private LayerMask layersToHit;
-    
-    
-    [SerializeField] private float distanceToSnap = 1;
-    [SerializeField] private float legSpeed = 1;
-    
-    [SerializeField] private float number;
+    [SerializeField] List<GameObject> thighHips;
+    [SerializeField] List<GameObject> legTargets;
 
-    private float _tick;
+    List<Vector3> _localRestPositions = new ();
+    List<Vector3> _snapPositions = new ();
+    List<Vector3> _rayDirections = new ();
+    List<Ray> _rays = new ();
+    Ray _tempRay;
+    RaycastHit _hit;
+    
+    [SerializeField] float maxRayDistance = 2;
+    [SerializeField] LayerMask layersToHit;
+    
+    [SerializeField] float distanceToSnap = 1;
+    // [SerializeField] float legSpeed = 1;
+    [SerializeField] float legTargetVariation = 0.1f;
+    [SerializeField] List<float> legStartOffsets = new() { 0f, 0.5f, 1f, -0.5f };
+    
+    // [SerializeField] private float number;
+    float _avgGroundPos;
 
-    private void Start()
+    float _tick;
+
+    void Start()
+    {
+        InitializeLegs();
+    }
+    
+    public void FixedUpdate()
+    {
+        Tick();
+        HuntPlayer();
+        
+        for (int i = 0; i < legTargets.Count; i++)
+        {
+            ProcessLeg(i);
+        }
+    }
+
+    void InitializeLegs()
     {
         for (int i = 0; i < legTargets.Count; i++)
         {
             _localRestPositions.Add(legTargets[i].transform.localPosition);
+            legTargets[i].transform.position += new Vector3(0, 0, legStartOffsets[i]);
             _snapPositions.Add(legTargets[i].transform.position);
             Vector3 rayDirection = (transform.TransformPoint(_localRestPositions[i]) - thighHips[i].transform.position).normalized;
             _rayDirections.Add(rayDirection);
@@ -38,15 +62,26 @@ public class ProceduralAnimation : MonoBehaviour
         }
     }
 
-    private void ProcessLeg(int i)
+    void Tick()
     {
         _tick += 1;
         if (_tick >= 60)
         {
             _tick = 0;
-            number = Random.Range(0f, 1f);
+            // number = Random.Range(-0.1f, 0.1f);
             //  + new Vector3(number, number, number)
         }
+    }
+
+    void HuntPlayer()
+    {
+        Vector3 direction = (player.transform.position - transform.position).normalized;
+        transform.position += direction * (speed * Time.fixedDeltaTime);
+        transform.LookAt(transform.position + direction);
+    }
+    
+    void ProcessLeg(int i)
+    {
         // variable positions, dancing, whatever
         // Vector3 adjustedRayDirection = _rayDirections[i] + transform.rotation.eulerAngles;
         // Vector3 adjustedRayDirection = _rayDirections[i];
@@ -58,28 +93,29 @@ public class ProceduralAnimation : MonoBehaviour
         _rays[i] = _tempRay;
         
         Debug.DrawRay(_rays[i].origin, _rayDirections[i], Color.red);
-        if (!Physics.Raycast(_rays[i], out _hit, maxDistance, layersToHit)) return;
+        if (!Physics.Raycast(_rays[i], out _hit, maxRayDistance, layersToHit)) return;
         
-        if (Vector3.Distance(_snapPositions[i], _hit.point) > distanceToSnap)
-        {
-            _snapPositions[i] = _hit.point;
-            // _snapPositions[i] += Vector3.Lerp(_snapPositions[i], _hit.point, Time.fixedDeltaTime * legSpeed);
-        }
-    }
 
-    public void FixedUpdate()
-    {
-        for (int i = 0; i < legTargets.Count; i++)
-        {
-            ProcessLeg(i);
-        }
+        if (!(Vector3.Distance(_snapPositions[i], _hit.point) > distanceToSnap)) return;
+        _snapPositions[i] = _hit.point + new Vector3(
+            Random.Range(-legTargetVariation, legTargetVariation),
+            0,
+            Random.Range(-legTargetVariation, legTargetVariation));
+            
+        // _snapPositions[i] += Vector3.Lerp(_snapPositions[i], _hit.point, Time.fixedDeltaTime * legSpeed);
     }
-
-    private void LateUpdate()
+    
+    void LateUpdate()
     {
+        _avgGroundPos = 0;
         for (int i = 0; i < legTargets.Count; i++)
         {
             legTargets[i].transform.position = _snapPositions[i];
+            _avgGroundPos += legTargets[i].transform.position.y;
         }
+
+        _avgGroundPos /= legTargets.Count;
+        // transform.position = new Vector3(transform.position.x, (_avgGroundPos - transform.position.y) * speed * Time.deltaTime, transform.position.z);
+        // transform.position = new Vector3(transform.position.x, _avgGroundPos, transform.position.z);
     }
 }
