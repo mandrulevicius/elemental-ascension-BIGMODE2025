00c02d8a8af85ad4eb001390a0bb653fbcfc529b
Minos <mindaugas.andrulevicius@gmail.com>
2025-01-30 01:58:55 +0200

    fixed spider, can climb now. still doesnt rotate body, but should be good enough

--- Assets/_MA/ProceduralAnimation.cs
+++ Assets/_MA/ProceduralAnimation.cs
@@ -1,15 +1,12 @@
 using System.Collections.Generic;
-using Unity.IntegerTime;
-using Unity.VisualScripting;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.Serialization;
 
 public class ProceduralAnimation : MonoBehaviour
 {
-    
-    [SerializeField] GameObject player;
-    [SerializeField] float speed = 5f;
-    Vector3 _movementDirection = Vector3.zero;
+    [SerializeField] LayerMask preyLayer;
+    GameObject _player;
     
     [SerializeField] GameObject body;
     
@@ -25,9 +22,13 @@ public class ProceduralAnimation : MonoBehaviour
     RaycastHit _hit;
 
     Ray _groundRay;
+    Ray _frontRay;
     
-    [SerializeField] float maxRayDistance = 2;
+    [FormerlySerializedAs("maxRayDistance")] [SerializeField] float maxLegReach = 2;
     [SerializeField] LayerMask layersToHit;
+    
+    [SerializeField] float speed = 5f;
+    Vector3 _movementDirection = Vector3.zero;
     
     [SerializeField] float distanceToSnap = 1;
     [SerializeField] float legTargetVariation = 0.1f;
@@ -37,20 +38,12 @@ public class ProceduralAnimation : MonoBehaviour
 
     void Start()
     {
-        InitializeLegs();
-    }
-    
-    public void FixedUpdate()
-    {
+        _player = GameObject.FindObjectsByType<GameObject>(FindObjectsInactive.Exclude, FindObjectsSortMode.None)
+            .FirstOrDefault(obj => ((1 << obj.layer) & preyLayer) != 0);
         
         _groundRay = new Ray(body.transform.position, Vector3.down);
-        Tick();
-        HuntPlayer();
-        
-        for (int i = 0; i < legTargets.Count; i++)
-        {
-            ProcessLeg(i);
-        }
+        _frontRay = new Ray(body.transform.position, Vector3.forward);
+        InitializeLegs();
     }
 
     void InitializeLegs()
@@ -65,6 +58,37 @@ public class ProceduralAnimation : MonoBehaviour
             _rays.Add(new Ray(thighHips[i].transform.position, rayDirection));
         }
     }
+    
+    public void FixedUpdate()
+    {
+        Tick();
+        HuntPlayer();
+        
+        for (int i = 0; i < legTargets.Count; i++)
+        {
+            ProcessLeg(i);
+        }
+        
+        // stay above ground
+        _groundRay.origin = body.transform.position;
+        _groundRay.direction = Vector3.down;
+        Debug.DrawRay(_groundRay.origin, _groundRay.direction, Color.red);
+        if (Physics.Raycast(_groundRay, out _hit, maxLegReach, layersToHit))
+        {
+            transform.position = _hit.point;
+        }
+        
+        _frontRay.origin = body.transform.position;
+        _frontRay.direction = _movementDirection;
+        Debug.DrawRay(_frontRay.origin, _frontRay.direction, Color.red);
+        if (Physics.Raycast(_frontRay, out _hit, maxLegReach, layersToHit))
+        {
+            // transform.LookAt(new Vector3(_hit.point.x, _hit.point.y + 1, _hit.point.z)); // delta time here
+        }
+        // look at front leg snap positions?
+    }
+
+
 
     void Tick()
     {
@@ -79,9 +103,9 @@ public class ProceduralAnimation : MonoBehaviour
 
     void HuntPlayer()
     {
-        _movementDirection = (player.transform.position - transform.position).normalized;
+        _movementDirection = (_player.transform.position - transform.position).normalized;
         transform.position += _movementDirection * (speed * Time.fixedDeltaTime);
-        transform.LookAt(transform.position + _movementDirection);
+        transform.LookAt(new Vector3(_player.transform.position.x, transform.position.y, _player.transform.position.z));
     }
     
     void ProcessLeg(int i)
@@ -90,6 +114,7 @@ public class ProceduralAnimation : MonoBehaviour
         // Vector3 adjustedRayDirection = _rayDirections[i] + transform.rotation.eulerAngles;
         // Vector3 adjustedRayDirection = _rayDirections[i];
 
+        // readjust rays according to current position
         _tempRay = _rays[i];
         _tempRay.origin = thighHips[i].transform.position;
         _rayDirections[i] = (transform.TransformPoint(_localRestPositions[i]) - _tempRay.origin).normalized;
@@ -97,30 +122,36 @@ public class ProceduralAnimation : MonoBehaviour
         _rays[i] = _tempRay;
         
         Debug.DrawRay(_rays[i].origin, _rayDirections[i], Color.red);
-        if (!Physics.Raycast(_rays[i], out _hit, maxRayDistance, layersToHit)) return;
+        if (!Physics.Raycast(_rays[i], out _hit, maxLegReach, layersToHit))
+        {
+            // if raycast miss, try closer to center
+            if (!Physics.Raycast(_rays[i].origin, (_rayDirections[i] + Vector3.down).normalized, out _hit, maxLegReach, layersToHit))
+            {
+                if (!Physics.Raycast(_rays[i].origin, Vector3.down, out _hit, maxLegReach, layersToHit)) return;
+            }
+        }
         
-
+        // if too far from resting position, move
         if (!(Vector3.Distance(_snapPositions[i], _hit.point) > distanceToSnap)) return;
-        _snapPositions[i] = _hit.point + _movementDirection + new Vector3(
+        _snapPositions[i] = _hit.point + _movementDirection * Random.Range(0f, 1f) + 
+                        new Vector3(
             Random.Range(-legTargetVariation, legTargetVariation),
             0,
             Random.Range(-legTargetVariation, legTargetVariation));
-            
+
         // _snapPositions[i] += Vector3.Lerp(_snapPositions[i], _hit.point, Time.fixedDeltaTime * legSpeed);
     }
     
     void LateUpdate()
     {
-        _groundRay.origin = body.transform.position;    
-        Debug.DrawRay(_groundRay.origin, _groundRay.direction, Color.red);
-        if (Physics.Raycast(_groundRay, out _hit, maxRayDistance, layersToHit))
-        {
-            transform.position = _hit.point;
-        }
-        
+        // snap legs
         for (int i = 0; i < legTargets.Count; i++)
         {
             legTargets[i].transform.position = _snapPositions[i];
         }
     }
 }
+
+// good enough for now, but fix climbing hills - IK on body? (i think wonky body rotation is interfering)
+
+// fix legs too synced. - good enough for now?
