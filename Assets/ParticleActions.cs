52ecc52cdc17c644a85ed94bc70bc4fed9a0513c
Minos <mindaugas.andrulevicius@gmail.com>
2025-02-01 21:15:16 +0200

    add projectile lifetime

--- Assets/ParticleActions.cs
+++ Assets/ParticleActions.cs
@@ -10,6 +10,8 @@ public class ParticleActions : MonoBehaviour
     [SerializeField] public float range= 5f;
     [SerializeField] public float speed= 5f;
     [SerializeField] public float damage = 1f;
+    [SerializeField] public int lifetimeTicks = 500;
+    int _lifetimeTick;
     
         private float _distastanceToEnemy;
         int _tick;
@@ -22,15 +24,16 @@ public class ParticleActions : MonoBehaviour
         [SerializeField] bool fullyGrown;
 
         [SerializeField] LayerMask friends;
-        
+        EntityStats entityStats;
+
         void Start()
     {
         
+        entityStats = GetComponent<EntityStats>();
         for (int j = 0; j < 32; j++) if ((friends.value & (1 << j)) != 0) Physics.IgnoreLayerCollision(gameObject.layer, j, true);  
         _startPosition = transform.position;
         _fullyGrownScale = transform.localScale;
         transform.localScale = Vector3.zero;
-        // add this to plants too
     }
         
 
@@ -42,14 +45,22 @@ public class ParticleActions : MonoBehaviour
         {
             stats.Health -= damage;
         }
-        GetComponent<EntityStats>().Health -= damage;
+        entityStats.Health -= damage;
         // should also explode when hits anything other than itself?
     }
 
     void FixedUpdate()
     {
         _tick += 1;
-        
+        _lifetimeTick += 1;
+        if (_lifetimeTick >= lifetimeTicks)
+        {
+            Destroy(gameObject, 1f);
+        }
+        else
+        {
+            if (fullyGrown) transform.localScale -= _fullyGrownScale / lifetimeTicks;
+        }
         
         if (_tick >= ticksUntilFullyGrown)
         {
