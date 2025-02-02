118ca54017ef32eeec273ba127300dd0c70458b4
Laimonas <pnlaimonas@gmail.com>
2025-02-02 22:16:23 +0200

    new arena

--- Assets/AudioScript.cs
+++ Assets/AudioScript.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 public class AudioScript : MonoBehaviour
 {
     public List<AudioClip> LandingAudioClips = new List<AudioClip>();
+    public AudioClip WanderingAudioClip;
+    public AudioClip FightingAudioClip;
+    private AudioSource audioSource;
+
+    public bool fight;
+    public bool wandering;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
+        setWandering();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+   public void setWandering()
+    {
+        wandering = true;
+        audioSource.clip = WanderingAudioClip;
+        audioSource.pitch = -0.5f;
+        audioSource.Play();
     }
 }
