66da9549d1bff06a3044ecc811693166950a8632
Minos <mindaugas.andrulevicius@gmail.com>
2025-02-02 22:12:13 +0200

    spider hit things

--- Assets/CollisionsRegister.cs
+++ Assets/CollisionsRegister.cs
@@ -6,16 +6,26 @@ public class CollisionsRegister : MonoBehaviour
     EntityStats playerStats; 
     public AudioClip HitAudioClip;
     public ParticleSystem HitParticles;
+    public GameObject HitGlowBall;
 
     private void OnTriggerEnter(Collider other)
     {
         int playerLayer = LayerMask.NameToLayer("Player");
-        if (other.gameObject.layer != playerLayer) return;
+        if (other.gameObject.layer != playerLayer)
+        {
+            return;
+        }
        playerStats =  other.gameObject.GetComponent<EntityStats>();
         if(HitAudioClip)
                AudioSource.PlayClipAtPoint(HitAudioClip, transform.position, 1);
         if (HitParticles)
             Instantiate(HitParticles, transform.position, Quaternion.identity);
+        if (HitGlowBall)
+        {
+            var glowBall = Instantiate(HitGlowBall, transform.position, Quaternion.identity);
+            Destroy(glowBall, 0.5f);
+        }
+        
         playerStats.Health -=1;
     }
 
