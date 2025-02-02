29f70e521c072d12258522874a2e7e9ec9f0f614
Minos <mindaugas.andrulevicius@gmail.com>
2025-02-02 04:21:51 +0200

    added assets for temple and mountain base - they dont need grass lets go

--- Assets/_MA/EntityStats.cs
+++ Assets/_MA/EntityStats.cs
@@ -38,18 +38,30 @@ public class EntityStats : MonoBehaviour
             {
                 dead = true;
                 OnDestruction?.Invoke();
-                if(deathEffectParticles != null)
-                Instantiate(deathEffectParticles, transform.position, Quaternion.identity);
+                if(deathEffectParticles)
+                    Instantiate(deathEffectParticles, transform.position, Quaternion.identity);
                 Destroy(gameObject, 0f);
                 // Destroy(selfPrefab, 0f);
             }
         }
     }
-    
     [SerializeField] public bool dead;
     public event Action OnDeath;
     public event Action OnDestruction;
     
+    
+    // [SerializeField] public multiplicativeModifier; // one slider to rule them all, affecting all relevant stats
+    // additiveModifier
+    // rageOnCondition x Friendlies died
+    // furyOnCondition y Friendlies died
+    // bloodlustOnCondition x Enemies killed
+    // frienzyOnCondition y Enemies killed
+    // reset counter
+    // gaining specific energy grants specific resource.
+    // on each entity death, send data to higher level layers of the program. Entity stats is mid-level local.
+    // need to have a global static class for each entity's Layers death tallies.
+    
+    // but first, just drop a pickup that increases the stat, and have it fly at player from anywhere or closest plants from their range.
     
     void Start()
     {
