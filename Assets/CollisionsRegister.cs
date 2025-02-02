6dbc3d50d15e35f479019e8021bcd600de3068aa
Minos <mindaugas.andrulevicius@gmail.com>
2025-02-02 21:53:21 +0200

    hit partilces

--- Assets/CollisionsRegister.cs
+++ Assets/CollisionsRegister.cs
@@ -5,18 +5,18 @@ public class CollisionsRegister : MonoBehaviour
 {
     EntityStats playerStats; 
     public AudioClip HitAudioClip;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-  
+    public ParticleSystem HitParticles;
 
     private void OnTriggerEnter(Collider other)
     {
         int playerLayer = LayerMask.NameToLayer("Player");
         if (other.gameObject.layer != playerLayer) return;
        playerStats =  other.gameObject.GetComponent<EntityStats>();
-if(HitAudioClip)
-       AudioSource.PlayClipAtPoint(HitAudioClip, transform.position, 1);
-
-       playerStats.Health -=1;
+        if(HitAudioClip)
+               AudioSource.PlayClipAtPoint(HitAudioClip, transform.position, 1);
+        if (HitParticles)
+            Instantiate(HitParticles, transform.position, Quaternion.identity);
+        playerStats.Health -=1;
     }
 
  
