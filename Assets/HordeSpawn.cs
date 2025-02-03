bfa6aeb559bafc16f34519da5811204a97fa5661
Laimonas <pnlaimonas@gmail.com>
2025-02-03 02:12:53 +0200

    New arena do horde mode

---
+++ Assets/HordeSpawn.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HordeSpawn : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public List<GameObject> _enemies = new List<GameObject>();
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/SpawnArenaCreeps.cs
+++ Assets/SpawnArenaCreeps.cs
@@ -1,17 +1,30 @@
 using System;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class SpawnArenaCreeps : MonoBehaviour
 {
     [SerializeField]GameObject arenaCreeps;
     [SerializeField] LayerMask playerLayer;
+    [SerializeField] bool hordeSpawn = false;
+    [SerializeField] float hordeSpawnTime = 50*60f;
     private void OnTriggerEnter(Collider other)
     {
         int player = LayerMask.NameToLayer("Player");
         if (other.gameObject.layer == player)
         {
-        Debug.Log(other.gameObject.name);
+            if (hordeSpawn)
+            {
+              var spawnedArena=  Instantiate(arenaCreeps, transform.position, Quaternion.identity);
+              spawnedArena.SetActive(true);
+              return;
+            }
             arenaCreeps.SetActive(true);
         }
+    }
+
+   void FixedUpdate()
+    {
+        
     }
 }
